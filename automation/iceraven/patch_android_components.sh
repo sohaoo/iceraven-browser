f7c14b140e267c110977c271887837e6b64721ce
akliuxingyuan <akliuxingyuan@gmail.com>
2023-09-29 22:30:18 +0800

    let startpage be general

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -1,2 +1,4 @@
 cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
 cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
+
+sed -i '42i\    "startpage",' android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
