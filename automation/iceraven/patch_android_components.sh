3a27fcd26b52740247764fc9b15641a83be4b495
akliuxingyuan <akliuxingyuan@gmail.com>
2023-10-03 15:17:18 +0800

    add brave search engine fix fenixDebug build

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -1,4 +1,10 @@
-cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
-cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
+#!/usr/bin/env bash
 
-sed -i '42i\    "startpage",' android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
+#cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
+#cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
+
+search_engines=( startpage brave )
+for engine in "${search_engines[@]}"
+do
+  sed -i "42i\    \"$engine\"," android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
+done
