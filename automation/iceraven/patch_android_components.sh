bd3f4ca813ff0cfc4d1064bc33058298214562eb
akliuxingyuan <akliuxingyuan@gmail.com>
2023-10-03 15:38:40 +0800

    uncommand it

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
-#cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
+cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
+cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
 
 search_engines=( startpage brave )
 for engine in "${search_engines[@]}"
