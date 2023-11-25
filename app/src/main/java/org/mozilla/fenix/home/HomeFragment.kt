166c9f981fd0ff36c192fb15337b6e6fb167f311
akliuxingyuan <akliuxingyuan@gmail.com>
2023-11-25 19:34:18 +0800

    fix wrong parameter position

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -478,8 +478,8 @@ class HomeFragment : Fragment() {
                         else -> true
                     }
                 },
-                showTopRecentSites = settings.showTopRecentSites,
             ),
+            showTopRecentSites = settings.showTopRecentSites,
         )
     }
 
