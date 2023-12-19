f12dece5b710c89e9df6198007787d9b4737444d
akliuxingyuan <akliuxingyuan@gmail.com>
2023-12-19 18:20:46 +0800

    update patch_android_components.sh

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -6,7 +6,7 @@ cp -f automation/iceraven/assets/list.json android-components/components/feature
 search_engines=( startpage brave )
 for engine in "${search_engines[@]}"
 do
-  sed -i "42i\    \"$engine\"," android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
+  sed -i "41i\    \"$engine\"," android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
 done
 
 git -C android-components apply < automation/iceraven/patches/top_sites_no_most_visted_sites.patch
