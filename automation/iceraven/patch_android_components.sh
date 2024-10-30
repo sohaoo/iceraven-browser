d986aff27bbfc7040c9b95acdf5c3d2cbd6dd052
akliuxingyuan <akliuxingyuan@gmail.com>
2024-10-30 20:07:35 +0800

    remove gleanPythonEnvDir settings

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+shopt -s globstar
+
 cp automation/iceraven/assets/*.xml android-components/components/feature/search/src/main/assets/searchplugins
 cp -f automation/iceraven/assets/list.json android-components/components/feature/search/src/main/assets/search
 
@@ -8,6 +10,8 @@ for engine in "${search_engines[@]}"
 do
   sed -i "41i\    \"$engine\"," android-components/components/feature/search/src/main/java/mozilla/components/feature/search/storage/SearchEngineReader.kt
 done
+
+sed -i "s#gleanPythonEnvDir#// gleanPythonEnvDir#g" android-components/**/*.gradle
 
 git -C android-components apply < automation/iceraven/patches/top_sites_no_most_visted_sites.patch
 git -C android-components apply < automation/iceraven/patches/suggestions_increase_number.patch
