961358b6a75c4231b1ebd672c64de31687bc3763
akliuxingyuan <akliuxingyuan@gmail.com>
2024-10-30 20:58:23 +0800

    ANDROID_BUILD_TOOLS_VERSION

--- automation/iceraven/install-sdk.sh
+++ automation/iceraven/install-sdk.sh
@@ -20,7 +20,7 @@ popd
 export ANDROID_SDK_ROOT=$HOME/android-sdk/android-sdk-linux
 
 # Install the weirdly missing NDK
-${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin/sdkmanager "ndk;27.0.12077973"
+${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin/sdkmanager "ndk;27.2.12479018"
 
 # Point the build at the tools
 echo "sdk.dir=${ANDROID_SDK_ROOT}" >> local.properties
